06acc29285f84127fa0bb4222d698ec9a64766a3
Kenan-LCF <kenan.lacrampe-cuyaubere-flieutete@epitech.eu>
2026-06-13 18:04:22 +0200

    Create level design

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -3,16 +3,9 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 5f;
-    private CapsuleCollider ColliderPlayer;
-    private BoxCollider Hitbox;
     public int direction = 1; 
     
 
-    void Start()
-    {
-        ColliderPlayer = GetComponent<CapsuleCollider>();
-        Hitbox = GetComponentInChildren<BoxCollider>();
-    }
 
     void Update()
     {
