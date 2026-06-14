af2aaad372d61da6a78c0657f61e6c9e592ededd
Kenan-LCF <kenan.lacrampe-cuyaubere-flieutete@epitech.eu>
2026-06-14 10:56:43 +0200

    added sprites for Doors and Checkpoint

--- Assets/Scripts/Interact/Door.cs
+++ Assets/Scripts/Interact/Door.cs
@@ -3,22 +3,31 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     BoxCollider2D boxCollider;
+    SpriteRenderer spriteRenderer;
+
+    public Sprite openSprite;
+    public Sprite closedSprite;
+
     public bool isOpen = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         ResetLevel.Instance.OnPlayerRespawn += CloseDoor;
     }
+
     public void OpenDoor()
     {
         isOpen = true;
         boxCollider.enabled = false;
-
+        spriteRenderer.sprite = openSprite;
     }
+
     public void CloseDoor()
     {
         isOpen = false;
         boxCollider.enabled = true;
+        spriteRenderer.sprite = closedSprite;
     }
 }
